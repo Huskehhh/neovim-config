26a9dfa4b48eeb0e79c5de121896f514faaa4597
Jordyn <jordyn@husk.pro>
2024-03-30 14:43:10 +1100

    fix: remove bad mappings

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -2,9 +2,6 @@ require "nvchad.mappings"
 
 local map = vim.keymap.set
 
-map("n", ";", ":", { desc = "CMD enter command mode" })
-map("i", "jk", "<ESC>")
-
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
 
 -- Nvim tree
