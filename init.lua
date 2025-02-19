60b662d6326c8cf0a4f99369fb43a925445069d7
Jordyn Newnham <jordyn@newnham.io>
2025-02-19 21:13:04 +1100

    chore: minor update to nvchad init

--- init.lua
+++ init.lua
@@ -18,9 +18,6 @@ require("lazy").setup({
     lazy = false,
     branch = "v2.5",
     import = "nvchad.plugins",
-    config = function()
-      require "options"
-    end,
   },
 
   { import = "plugins" },
@@ -29,6 +26,7 @@ require("lazy").setup({
 dofile(vim.g.base46_cache .. "defaults")
 dofile(vim.g.base46_cache .. "statusline")
 
+require "options"
 require "nvchad.autocmds"
 
 vim.schedule(function()
